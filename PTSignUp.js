567d5a211ca0a192dccb4bbca68f74d3ac1bc930
BCCC0 <52162254+BCCC0@users.noreply.github.com>
2022-06-09 16:20:53 -0700

    Create PTSignUp.js

---
+++ PTSignUp.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name         PTSignUp
+// @namespace    http://tampermonkey.net/
+// @author       You
+// @match        YOUR_PT_SIGN_UP_PAGE
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    window.setTimeout(()=>{location.reload();}, 24 * 60 * 60 * 1000);
+})();
